b6345f6b10625a5650a9f9c5e8d8cb13ed65ab0c
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-22 22:24:07 +0000

    Initial commit

---
+++ src/main/java/com/example/socialmediafollowservice/SocialMediaFollowServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.socialmediafollowservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaFollowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaFollowServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socialmediafollowservice/SocialMediaFollowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socialmediafollowservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaFollowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
