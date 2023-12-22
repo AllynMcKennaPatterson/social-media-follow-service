5aef053f4e73830ce07d66e36159402146a14128
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-22 22:35:53 +0000

    Created FollowRepository

---
+++ src/main/java/com/example/socialmediafollowservice/Interfaces/FollowRepository.java
@@ -0,0 +1,7 @@
+package com.example.socialmediafollowservice.Interfaces;
+
+import com.example.socialmediafollowservice.Models.UserFollowing;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface FollowRepository extends MongoRepository<UserFollowing, String> {
+}
