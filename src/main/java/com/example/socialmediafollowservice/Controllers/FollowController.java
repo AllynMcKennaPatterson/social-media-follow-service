b81ba7a6ff33a93ff85a0f4a8ed2cbc6ca7da020
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-27 13:38:20 +0000

    Added method for initialising a user's followlist

--- src/main/java/com/example/socialmediafollowservice/Controllers/FollowController.java
+++ src/main/java/com/example/socialmediafollowservice/Controllers/FollowController.java
@@ -3,6 +3,8 @@ package com.example.socialmediafollowservice.Controllers;
 import com.example.socialmediafollowservice.Interfaces.FollowRepository;
 import com.example.socialmediafollowservice.Models.UserFollowing;
 import com.example.socialmediafollowservice.Services.FollowService;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.bson.json.JsonObject;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +16,6 @@ import java.util.Map;
 public class FollowController {
     FollowService followService;
     FollowRepository followRepository;
-
     public FollowController(FollowService followService, FollowRepository followRepository){
         this.followService = followService;
         this.followRepository = followRepository;
@@ -26,4 +27,13 @@ public class FollowController {
         response.put("Follow user", followService.addFollow(userFollowing));
         return response;
     }
+
+    @PostMapping("/initFollowList")
+    Map<String, Boolean> initFollowList(@RequestBody String username){
+        Map<String,Boolean> response = new HashMap<>();
+        response.put("Initialised follow list", followService.initFollow(username));
+        return response;
+    }
+
+
 }
