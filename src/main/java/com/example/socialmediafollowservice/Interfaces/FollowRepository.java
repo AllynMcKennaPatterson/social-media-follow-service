87deff757425ba9bf897014a15748841bce6904f
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-27 13:37:58 +0000

    Added queries

--- src/main/java/com/example/socialmediafollowservice/Interfaces/FollowRepository.java
+++ src/main/java/com/example/socialmediafollowservice/Interfaces/FollowRepository.java
@@ -1,8 +1,17 @@
 package com.example.socialmediafollowservice.Interfaces;
 
 import com.example.socialmediafollowservice.Models.UserFollowing;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface FollowRepository extends MongoRepository<UserFollowing, String> {
+    Boolean existsByMyUsername(String myUsername);
+
+    Boolean existsByFollowListContainingAndMyUsername(String username, String myUsername);
+
+    Optional<UserFollowing> getUserFollowingByMyUsername(String myUsername);
 
 }
