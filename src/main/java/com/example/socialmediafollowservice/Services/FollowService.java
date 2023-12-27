62d3d81373c90881a1187a018d223971292563ef
Allyn McKenna Patterson <allyncodes@gmail.com>
2023-12-27 13:39:47 +0000

    Added method for initialising a user's followlist Added error checks when adding a username to a user's followlist Changed logic to update a user's followlist instead of adding a new document

--- src/main/java/com/example/socialmediafollowservice/Services/FollowService.java
+++ src/main/java/com/example/socialmediafollowservice/Services/FollowService.java
@@ -2,28 +2,60 @@ package com.example.socialmediafollowservice.Services;
 
 import com.example.socialmediafollowservice.Interfaces.FollowRepository;
 import com.example.socialmediafollowservice.Models.UserFollowing;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Service
 public class FollowService {
 
     FollowRepository followRepository;
+    MongoTemplate mongoTemplate;
 
-    public FollowService(FollowRepository followRepository) {
+    public FollowService(FollowRepository followRepository, MongoTemplate mongoTemplate) {
         this.followRepository = followRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public Boolean addFollow(UserFollowing userFollowing){
-        ArrayList<String> currentFollowList;
-        UserFollowing followListToDb = new UserFollowing(userFollowing.getId(), userFollowing.getMyUsername(), userFollowing.getFollowList());
-        currentFollowList = followListToDb.getFollowList();
-        currentFollowList.add(userFollowing.getUserToFollow());
-        followListToDb.setFollowList(currentFollowList);
-        followRepository.save(followListToDb);
-        System.out.println("User (" + userFollowing.getUserToFollow() + ") added to " + followListToDb.getMyUsername() + "'s follow list\n");
-        return true;
+        if(followRepository.existsByMyUsername(userFollowing.getMyUsername())) {
+            if (!followRepository.existsByFollowListContainingAndMyUsername(userFollowing.getUserToFollow(), userFollowing.getMyUsername())) {
+                Query query = new Query();
+                query.addCriteria(Criteria.where("myUsername").is(userFollowing.getMyUsername()));
+                Update update = new Update();
+                update.addToSet("followList", userFollowing.getUserToFollow());
+                mongoTemplate.updateFirst(query, update, "following");
+                System.out.println("User (" + userFollowing.getUserToFollow() + ") added to " + userFollowing.getMyUsername() + "'s follow list");
+                return true;
+            } else {
+                System.out.println("User (" + userFollowing.getUserToFollow() + ") already exists in " + userFollowing.getMyUsername() + "'s follow list");
+                return false;
+            }
+        }
+        else{
+            System.out.println("Error: User does not exist in database");
+            return false;
+        }
+    }
+
+    public Boolean initFollow(String username){
+        if(!followRepository.existsByMyUsername(username)){
+            ArrayList<String> emptyFollowList = new ArrayList<>();
+            UserFollowing newFollowList = new UserFollowing(username, emptyFollowList);
+            followRepository.save(newFollowList);
+            System.out.println("Initialised follow list for user: " + username + " \n");
+            return true;
+        }
+        else{
+            System.out.println("User (" + username + ") already exists");
+            return false;
+        }
+
     }
 
 }
